62412d004e87868a3693cc8be48b2739451d5b78
letal32 <marco.stellin@tecnico.ulisboa.pt>
2017-11-28 11:49:48 +0000

    test login

--- application.py
+++ application.py
@@ -1,7 +1,83 @@
 from flask import Flask
+from flask import render_template
+from flask import Response
+import json
+from flask import session
+import fenixedu
+
 
 application = Flask(__name__)
 
+application.config['REDIRECT_URI'] = "/authorized"
+application.config['CLIENT_ID'] = "1132965128044586"
+application.config['CLIENT_SECRET'] = "pUJJ1hK2COuTjwurjP6TiZhgXFEVo+dm5dGivY2b7WQGDy+/0tOdrkscT2wSmP0/kBwxj8HnnqgXqoOw7t0eFg=="
+application.config['BASE_URL'] = ""
+application.config['DEBUG'] = True
+
+
+@application.route('/')
+def index():
+	access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+        
+    return "Successfully Logged In"
+
+
+
+@application.route(application.config['REDIRECT_URI'])
+def user_auth():
+	code = request.args.get('code')
+	config = fenixedu.FenixEduConfiguration(application.config['CLIENT_ID'], 
+											application.config['REDIRECT_URI'],
+											application.config['CLIENT_SECRET'],
+											application.config['BASE_URL'])
+
+	client = fenixedu.FenixEduClient(config)
+	user = client.get_user_by_code(code)
+	session['access_token'] = user.access_token
+
+	return redirect(url_for('index'))
+
+
+@application.route('/login')
+def user_login():
+	config = fenixedu.FenixEduConfiguration(application.config['CLIENT_ID'], 
+											application.config['REDIRECT_URI'],
+											application.config['CLIENT_SECRET'],
+											application.config['BASE_URL'])
+
+	client = fenixedu.FenixEduClient(config)
+	url = client.get_authentication_url()
+
+	return render_template('login.html', url=url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+counter = 0
+
+
 @application.route('/')
 def hello_world():
-    return 'Hello, World!'
+	message = "Hello World!"
+	return render_template('index.html', message=message)
+
+@application.route('/increment')
+def increment():
+	global counter 
+	counter += 1
+	dic = {'count': counter}
+
+	return Response(json.dumps(dic), mimetype="application/json")
+"""
