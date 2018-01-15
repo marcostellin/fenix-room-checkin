bd1cac241079c2cc27b16e0366351d3121ee64ee
Marco Stellin <letal32@gmail.com>
2018-01-15 23:36:11 +0100

    New message functionality with Alertify JS

--- static/get_messages.js
+++ static/get_messages.js
@@ -2,14 +2,18 @@
  
             $.getJSON("/user/ajax/messages", function(resp){
                 for (var i=0; i<resp.length; i++){
+                    var flag = false;
+                    
                     alertify.alert("Message from " + resp[i]["from"], resp[i]["content"],
                     function (){
                        $.post("/user/ajax/messages", {read:"True"});
+                       flag = true;
                     });
+                    
+                    while (!flag){};
                 }
 
             });
-
+            
             setTimeout(worker, 5000);
-
 });
