d4b02675fcb9bca010b913087edb0e2f186791e5
Marco Stellin <letal32@gmail.com>
2018-01-16 00:23:36 +0100

    New message implementation

--- static/get_messages.js
+++ static/get_messages.js
@@ -1,16 +1,21 @@
  $(document).ready(function worker() {
- 
+            
+            var flag = false;
             $.getJSON("/user/ajax/messages", function(resp){
-
+                    
+                    flag = true;
                     alertify.alert("Message from " + resp["from"], resp["content"],
                     function (){
                        $.post("/user/ajax/messages", {read:"True"});
+                       flag = false;
+                       setTimeout(worker, 5000);
                     })
 
                 
-            })
-              .always(function (){
-                  setTimeout(worker, 5000);
-              });
+            });
+            
+            if (!flag){
+                setTimeout(worker, 5000);
+            }
            
 });
